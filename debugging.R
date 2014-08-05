2d7f8966268c4f8c6ca1363de7e5a599610b1c33
yilinx <yilinx@gmail.com>
2014-08-06 07:33:55 +0800

    final version v1

--- debugging.R
+++ debugging.R
@@ -10,5 +10,25 @@ testing = adData[-inTrain,]
 
 set.seed(62433) # simulate randomforest as the first model to be constructed
 modFit2.rf2 <- train(diagnosis~., methood="rf", data=training)
+modFit2.gbm <- train(diagnosis~., methood="gbm", data=training)
+modFit2.lda <- train(diagnosis~., methood="lda", data=training)
+
+stack.rf <- predict(modFit2.rf2, training)
+stack.gbm <- predict(modFit2.gbm, training)
+stack.lda <- predict(modFit2.lda, training)
+stack.df <- data.frame(training$diagnosis, stack.rf, stack.gbm, stack.lda)
+
+modFit2.stack <- train(training.diagnosis~., methood="rf", data=stack.df)
+
+testing.stack <- data.frame("stack.rf"=predict(modFit2.rf2,testing),
+                            "stack.gbm"=predict(modFit2.gbm,testing),
+                            "stack.lda"=predict(modFit2.lda,testing))
+
 accuracy2.rf2 <- confusionMatrix(testing$diagnosis, predict(modFit2.rf2,testing))
 accuracy2.rf2$overall[1]
+accuracy2.gbm <- confusionMatrix(testing$diagnosis, predict(modFit2.gbm,testing))
+accuracy2.gbm$overall[1]
+accuracy2.lda <- confusionMatrix(testing$diagnosis, predict(modFit2.lda,testing))
+accuracy2.lda$overall[1]
+accuracy2.stack <- confusionMatrix(testing$diagnosis, predict(modFit2.stack,testing.stack))
+accuracy2.stack$overall[1]
